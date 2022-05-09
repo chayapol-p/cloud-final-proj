4e75a50b5dfa39cf2e0ecb7f8c0c15f3fa9c55e0
thirakhunrak <thirakhunrak@hotmail.com>
2022-05-09 20:31:19 +0700

    add textract code

--- server/cdk/lambda/lambda-handler.py
+++ server/cdk/lambda/lambda-handler.py
@@ -5,12 +5,30 @@ import boto3
 
 
 def main(event, context):
+
+    dump = json.dumps(event)
+    userInput = json.loads(dump)["queryStringParameters"]
+
     random_value = str(uuid.uuid4().hex)
     TABLE_NAME = os.environ.get('TABLE_NAME')
     REGION = os.environ.get('REGION')
     print(f'Random value is {random_value}')
 
+    textractclient = boto3.client("textract", region_name=REGION)
     dynamo_client = boto3.client("dynamodb", region_name=REGION)
+
+    # textract
+    textract_response = textractclient.detect_document_text(
+        Document={
+            'Bytes': userInput['card']
+        }
+    )
+
+    extractedText = ""
+    for block in textract_response['Blocks']:
+        if block["BlockType"] == "LINE":
+            extractedText = extractedText+block["Text"]+" "
+    ####
 
     # data = dynamo_client.get_item(TableName=TABLE_NAME, Key={"username": {"S": username}})
     data = dynamo_client.get_item(TableName=TABLE_NAME,)
@@ -21,5 +39,5 @@ def main(event, context):
 
     print("Succeeded:")
     print(json.dumps(response, indent=2))
-    
+
     return json.dumps(response, indent=2)
