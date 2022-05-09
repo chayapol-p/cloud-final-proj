7602f78e288f485fbbede3959c74e6622a1fc283
Chayapol Parisonyu <chayapol.par@gmail.com>
2022-05-09 18:19:28 +0700

    Draft All Resource

---
+++ server/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+from aws_cdk import aws_rds as rds
+
+from cdk.cdk_stack import FinalProjStack
+
+
+config = {
+    'account_id':"402007393381",
+    'region':"ap-southeast-1",
+}
+
+stack_config = cdk.Environment(account=config["account_id"], region=config["region"])
+
+stack_params = {
+    "env": stack_config,
+    "region": config["region"],
+}
+
+app = cdk.App()
+
+FinalProjStack(app, "Cloud-Final", **stack_params)
+
+app.synth()
---
+++ server/cdk/cdk_stack.py
@@ -0,0 +1,45 @@
+from aws_cdk import (
+    Stack,
+    aws_lambda as lambda_,
+    aws_dynamodb as dynamodb,
+)
+import aws_cdk
+from constructs import Construct
+
+import os
+
+dirname = os.path.dirname(__file__)
+
+class FinalProjStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, region: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        table_name = "FinalProjectData"
+
+        lambda_fn = lambda_.Function(
+            self,
+            "FinalProject",
+            code=lambda_.Code.from_asset(os.path.join(dirname, "lambda")),
+            handler="lambda-handler.main",
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            environment={"TABLE_NAME": table_name, "REGION": region},
+        )
+
+        # Build our DynamoDb Table
+        dynamo = dynamodb.Table(
+            self,
+            table_name,
+            table_name=table_name,
+            partition_key=dynamodb.Attribute(
+                name="user_id", type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="resource_key", type=dynamodb.AttributeType.STRING
+            ),
+            removal_policy=aws_cdk.RemovalPolicy.DESTROY,
+            billing_mode=dynamodb.BillingMode.PROVISIONED,
+            read_capacity=2,
+            write_capacity=2,
+        )
+        
+        dynamo.grant_full_access(lambda_fn)
